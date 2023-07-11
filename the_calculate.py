6332c664eac0ae2a683cde81964a6b95fe6732aa
Exin_mood <mohygang988@gmail.com>
2023-07-11 10:01:16 +0430

    none

--- the_calculate.py
+++ the_calculate.py
@@ -60,10 +60,7 @@ def insertequal():
 
 
 def insertclean(labl):
-    global fuck
-    javab.config(text=fu)
-    javab.update()
-    
+    pass
     
 #one to nine
 btn1=Button(window,text='1',font='tahoma 15',padx=10 , pady=10 , bd='4',command=lambda:insertnumber(1))
